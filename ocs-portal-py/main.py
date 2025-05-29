ecca0cafb60b6732180cacbea6f36c8902c773f5
CrypticSYS <cthompson@ocboe.com>
2025-05-29 12:47:11 -0500

    add inventory html page

--- ocs-portal-py/main.py
+++ ocs-portal-py/main.py
@@ -19,3 +19,13 @@ def new_tech_ticket(request: Request):
 @app.get("/tickets/maintenance/new")
 def new_maintenance_ticket(request: Request):
     return templates.TemplateResponse("new_maintenance_ticket.html", {"request": request})
+
+@app.get("/inventory/add")
+def add_inventory_form(request: Request):
+    return templates.TemplateResponse("add_inventory.html", {"request": request})
+
+@app.post("/inventory/add")
+def add_inventory_submit(request: Request):
+    # TODO: Process form data and save to database
+    # For now, just redirect to home or show a success message
+    return RedirectResponse("/", status_code=303)
