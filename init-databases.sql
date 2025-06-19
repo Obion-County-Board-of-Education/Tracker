e3561711d387b261dc0be009bf561c6b62bb711a
VictoryTek <jhowell@ocboe.com>
2025-06-19 14:02:00 -0500

    removed inventory, and moved to manage

--- init-databases.sql
+++ init-databases.sql
@@ -5,9 +5,6 @@
 -- Create ocs_tickets database
 SELECT 'CREATE DATABASE ocs_tickets' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ocs_tickets')\gexec
 
--- Create ocs_inventory database
-SELECT 'CREATE DATABASE ocs_inventory' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ocs_inventory')\gexec
-
 -- Create ocs_purchasing database
 SELECT 'CREATE DATABASE ocs_purchasing' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ocs_purchasing')\gexec
 
@@ -19,7 +16,6 @@ SELECT 'CREATE DATABASE ocs_forms' WHERE NOT EXISTS (SELECT FROM pg_database WHE
 
 -- Grant permissions to ocs_user on all databases
 GRANT ALL PRIVILEGES ON DATABASE ocs_tickets TO ocs_user;
-GRANT ALL PRIVILEGES ON DATABASE ocs_inventory TO ocs_user;
 GRANT ALL PRIVILEGES ON DATABASE ocs_purchasing TO ocs_user;
 GRANT ALL PRIVILEGES ON DATABASE ocs_portal TO ocs_user;
 GRANT ALL PRIVILEGES ON DATABASE ocs_manage TO ocs_user;
